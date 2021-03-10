2fab67e22342e928e1ae3f91e5e5c991bebc1b73
coord.e <me@coord-e.com>
2021-03-10 16:05:38 +0000

    Change: Update health check to check table grants

--- sos21-database/src/query/is_healthy.rs
+++ sos21-database/src/query/is_healthy.rs
@@ -1,15 +1,46 @@
+use std::convert::TryInto;
+
 use anyhow::{Context, Result};
 
 pub async fn is_healthy<'a, E>(conn: E) -> Result<bool>
 where
     E: sqlx::Executor<'a, Database = sqlx::Postgres>,
 {
-    let not_in_sync = sqlx::query_scalar!(
-        r#"SELECT coalesce(bool_or(needs_sync), false) as "not_in_sync!" FROM forms"#
+    let table_names: &[&str] = [
+        "users",
+        "projects",
+        "forms",
+        "form_condition_includes",
+        "form_condition_excludes",
+        "form_project_query_conjunctions",
+        "form_answers",
+    ]
+    .as_ref();
+
+    let table_names_len: i64 = table_names.len().try_into()?;
+
+    let has_grants = sqlx::query_scalar!(
+        r#"
+WITH grants AS (
+    SELECT
+        array_agg(privilege_type::text) AS privilege_types,
+        table_name::text
+    FROM information_schema.role_table_grants
+    WHERE grantee = current_user AND table_name::text = ANY ($1)
+    GROUP BY table_name
+)
+SELECT
+    (bool_and(grants.privilege_types @> ARRAY['DELETE', 'UPDATE', 'SELECT', 'INSERT'])
+        AND count(grants.table_name) = $2
+    ) AS "has_grants!"
+FROM grants
+"#,
+        table_names as _,
+        table_names_len
     )
     .fetch_one(conn)
     .await
-    .context("Failed to select from forms")?;
+    .context("Failed to select from role_table_grants")?;
 
-    Ok(!not_in_sync)
+    Ok(has_grants)
 }
