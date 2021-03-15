596976d46af1a555e9d530f92b98cc43b0af1fdd
coord.e <me@coord-e.com>
2021-03-15 09:27:29 +0000

    Change: Return millisecond timestamp from API server

--- sos21-api-server/src/handler/model/file.rs
+++ sos21-api-server/src/handler/model/file.rs
@@ -1,6 +1,6 @@
+use crate::handler::model::date_time::DateTime;
 use crate::handler::model::user::UserId;
 
-use chrono::{DateTime, Utc};
 use mime::Mime;
 use serde::{Deserialize, Serialize};
 use sos21_use_case::model::file as use_case;
@@ -23,7 +23,7 @@ impl FileId {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct File {
     pub id: FileId,
-    pub created_at: DateTime<Utc>,
+    pub created_at: DateTime,
     pub author_id: UserId,
     pub name: Option<String>,
     #[serde(with = "serde_mime", rename = "type")]
@@ -37,7 +37,7 @@ impl File {
     pub fn from_use_case(file: use_case::File) -> Self {
         File {
             id: FileId::from_use_case(file.id),
-            created_at: file.created_at,
+            created_at: DateTime::from_use_case(file.created_at),
             author_id: UserId::from_use_case(file.author_id),
             name: file.name,
             type_: file.type_,
