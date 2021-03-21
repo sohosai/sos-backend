73bb9b739d664039d77868ccbed04bf1482de5e1
coord.e <me@coord-e.com>
2021-03-21 09:37:55 +0000

    Change: Bump the version numbers

--- default.nix
+++ default.nix
@@ -2,10 +2,10 @@
 
 pkgs.rustPlatform.buildRustPackage {
   pname = "sos21-backend";
-  version = "0.1.0";
+  version = "0.2.0";
   src = ./.;
 
-  cargoSha256 = "1q8vb047z9dmxmajvvlbr3anwm87h1s0jgay3yvmjkxvb2zrksb3";
+  cargoSha256 = "1dyqh7larb4rm4qc0swp6y4ffrhhv6lzx6nlrmbpkvq5lqb8d18q";
 
   nativeBuildInputs = with pkgs; [
     zlib
