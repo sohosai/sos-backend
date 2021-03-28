00e8d314eedcebb875ccf1d3359860e28ae7cc32
coord.e <me@coord-e.com>
2021-03-28 17:56:57 +0000

    Change: Update warp to 0.3.1

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ pkgs.rustPlatform.buildRustPackage {
   version = "0.2.1";
   src = ./.;
 
-  cargoSha256 = "020252zd06g1vl24zh0z86xd50x8cknyrw3ibg5bbmh1z0x6qv7f";
+  cargoSha256 = "1idrfs6avjm6cx75mx13pa9syjsb5q9xx1ywi4v1rlkgzzffnh5m";
 
   nativeBuildInputs = with pkgs; [
     zlib
