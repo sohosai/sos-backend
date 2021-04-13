049632dceea2443cbb3e49a1c5b9d5f702d174aa
coord.e <me@coord-e.com>
2021-04-13 11:42:41 +0000

    Change: Manually declare nix-shell dependencies

--- shell.nix
+++ shell.nix
@@ -1,15 +1,17 @@
 { pkgs ? import ./nix/pkgs.nix }:
 let
-  sos21-backend = import ./. { inherit pkgs; };
   sqlx-cli = pkgs.callPackage ./nix/sqlx-cli.nix { };
   crate2nix = builtins.fetchTarball {
     url = "https://github.com/kolloch/crate2nix/tarball/0.9.0";
     sha256 = "0lxi9zl5mzzpz7gsa7pbqag163hv84xd6diyj0f0y5whf1mk07vl";
   };
 in
-sos21-backend.overrideAttrs (oldAttrs: {
-  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    zlib
+    rustc
+    cargo
     sqlx-cli
     crate2nix
   ];
-})
+}
