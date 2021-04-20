3ae869547a239c6c1e128fd15b2324e6bc2c9f02
coord.e <me@coord-e.com>
2021-04-20 14:20:39 +0000

    Change: Require stripped input in the texts of user model

--- sos21-domain/src/model/user/affiliation.rs
+++ sos21-domain/src/model/user/affiliation.rs
@@ -1,10 +1,10 @@
-use crate::model::string::LengthBoundedString;
+use crate::model::string::{self, LengthBoundedString, StrippedString};
 
 use thiserror::Error;
 
 /// A text of users' affiliation, whose length is 1 ..= 128 chars.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct UserAffiliation(LengthBoundedString<typenum::U1, typenum::U128, String>);
+pub struct UserAffiliation(StrippedString<LengthBoundedString<typenum::U1, typenum::U128, String>>);
 
 #[derive(Debug, Error, Clone)]
 #[error("invalid user affiliation")]
@@ -12,14 +12,26 @@ pub struct AffiliationError {
     _priv: (),
 }
 
+impl AffiliationError {
+    fn from_length_error(_err: string::BoundedLengthError<typenum::U1, typenum::U128>) -> Self {
+        AffiliationError { _priv: () }
+    }
+
+    fn from_not_stripped_error(_err: string::NotStrippedError) -> Self {
+        AffiliationError { _priv: () }
+    }
+}
+
 impl UserAffiliation {
     pub fn from_string(affiliation: impl Into<String>) -> Result<Self, AffiliationError> {
         let inner = LengthBoundedString::new(affiliation.into())
-            .map_err(|_| AffiliationError { _priv: () })?;
+            .map_err(AffiliationError::from_length_error)?;
+        let inner =
+            StrippedString::new(inner).map_err(AffiliationError::from_not_stripped_error)?;
         Ok(UserAffiliation(inner))
     }
 
     pub fn into_string(self) -> String {
-        self.0.into_inner()
+        self.0.into_inner().into_inner()
     }
 }
