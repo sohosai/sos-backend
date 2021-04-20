ebdbd5f1b4743c6280bfb9ef3e3c5d538dc8f395
coord.e <me@coord-e.com>
2021-04-20 14:52:09 +0000

    Change: Require stripped input in the texts of file distribution model

--- sos21-domain/src/model/file_distribution/name.rs
+++ sos21-domain/src/model/file_distribution/name.rs
@@ -1,9 +1,11 @@
-use crate::model::string::{self, LengthBoundedString};
+use crate::model::string::{self, LengthBoundedString, StrippedString};
 
 use thiserror::Error;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct FileDistributionName(LengthBoundedString<typenum::U1, typenum::U64, String>);
+pub struct FileDistributionName(
+    StrippedString<LengthBoundedString<typenum::U1, typenum::U64, String>>,
+);
 
 #[derive(Debug, Error, Clone)]
 #[error("invalid file distribution name")]
@@ -15,19 +17,24 @@ impl NameError {
     fn from_length_error(_err: string::BoundedLengthError<typenum::U1, typenum::U64>) -> Self {
         NameError { _priv: () }
     }
+
+    fn from_not_stripped_error(_err: string::NotStrippedError) -> Self {
+        NameError { _priv: () }
+    }
 }
 
 impl FileDistributionName {
     pub fn from_string(name: impl Into<String>) -> Result<Self, NameError> {
         let inner = LengthBoundedString::new(name.into()).map_err(NameError::from_length_error)?;
+        let inner = StrippedString::new(inner).map_err(NameError::from_not_stripped_error)?;
         Ok(FileDistributionName(inner))
     }
 
     pub fn as_str(&self) -> &str {
-        self.0.as_ref()
+        self.0.as_str()
     }
 
     pub fn into_string(self) -> String {
-        self.0.into_inner()
+        self.0.into_inner().into_inner()
     }
 }
