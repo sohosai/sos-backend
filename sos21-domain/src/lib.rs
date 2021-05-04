77e7fa0e9af037cfdabb0eb98ac69ecf9ff9fcae
coord.e <me@coord-e.com>
2021-05-04 14:20:49 +0000

    Add: Add domain_ensure for DomainResult

--- sos21-domain/src/lib.rs
+++ sos21-domain/src/lib.rs
@@ -1,3 +1,15 @@
+macro_rules! domain_ensure {
+    ($cond:expr) => {
+        if !$cond {
+            return Err($crate::DomainError::Internal(::anyhow::anyhow!(concat!(
+                "Condition failed: `",
+                stringify!($cond),
+                "`"
+            ))));
+        }
+    };
+}
+
 pub mod context;
 pub mod model;
 
