bc9c6eefcacff6e1fd066abf192320c7deab242c
coord.e <me@coord-e.com>
2021-05-12 18:26:26 +0000

    Fix: Fix shell.nix style

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import ./nix/pkgs.nix }:
 let
   sqlx-cli = pkgs.callPackage ./nix/sqlx-cli.nix { };
-  crate2nix = import ./nix/crate2nix.nix {};
+  crate2nix = import ./nix/crate2nix.nix { };
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
