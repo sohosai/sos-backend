22c8a21ec9c8d015fc7facd0513da6e484bcf117
coord.e <me@coord-e.com>
2021-05-22 06:26:10 +0000

    Change: Use update_project for projec/update

--- sos21-api-server/src/handler/project/update.rs
+++ sos21-api-server/src/handler/project/update.rs
@@ -4,7 +4,7 @@ use crate::handler::{HandlerResponse, HandlerResult};
 
 use serde::{Deserialize, Serialize};
 use sos21_domain::context::Login;
-use sos21_use_case::update_any_project;
+use sos21_use_case::update_project;
 use warp::http::StatusCode;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -43,6 +43,7 @@ pub enum Error {
     ProjectNotFound,
     InsufficientPermissions,
     DuplicatedProjectAttributes,
+    OutOfProjectCreationPeriod,
     InvalidField { field: &'static str },
 }
 
@@ -52,37 +53,37 @@ impl HandlerResponse for Error {
             Error::ProjectNotFound => StatusCode::NOT_FOUND,
             Error::InsufficientPermissions => StatusCode::FORBIDDEN,
             Error::DuplicatedProjectAttributes => StatusCode::BAD_REQUEST,
+            Error::OutOfProjectCreationPeriod => StatusCode::CONFLICT,
             Error::InvalidField { .. } => StatusCode::BAD_REQUEST,
         }
     }
 }
 
-impl From<update_any_project::Error> for Error {
-    fn from(err: update_any_project::Error) -> Error {
+impl From<update_project::Error> for Error {
+    fn from(err: update_project::Error) -> Error {
         match err {
-            update_any_project::Error::NotFound => Error::ProjectNotFound,
-            update_any_project::Error::InsufficientPermissions => Error::InsufficientPermissions,
-            update_any_project::Error::InvalidName => Error::InvalidField { field: "name" },
-            update_any_project::Error::InvalidKanaName => {
-                Error::InvalidField { field: "kana_name" }
-            }
-            update_any_project::Error::InvalidGroupName => Error::InvalidField {
+            update_project::Error::NotFound => Error::ProjectNotFound,
+            update_project::Error::InsufficientPermissions => Error::InsufficientPermissions,
+            update_project::Error::OutOfCreationPeriod => Error::OutOfProjectCreationPeriod,
+            update_project::Error::InvalidName => Error::InvalidField { field: "name" },
+            update_project::Error::InvalidKanaName => Error::InvalidField { field: "kana_name" },
+            update_project::Error::InvalidGroupName => Error::InvalidField {
                 field: "group_name",
             },
-            update_any_project::Error::InvalidKanaGroupName => Error::InvalidField {
+            update_project::Error::InvalidKanaGroupName => Error::InvalidField {
                 field: "kana_group_name",
             },
-            update_any_project::Error::InvalidDescription => Error::InvalidField {
+            update_project::Error::InvalidDescription => Error::InvalidField {
                 field: "description",
             },
-            update_any_project::Error::DuplicatedAttributes => Error::DuplicatedProjectAttributes,
+            update_project::Error::DuplicatedAttributes => Error::DuplicatedProjectAttributes,
         }
     }
 }
 
 #[apply_macro::apply(handler)]
 pub async fn handler(ctx: Login<Context>, request: Request) -> HandlerResult<Response, Error> {
-    let input = update_any_project::Input {
+    let input = update_project::Input {
         id: request.id.into_use_case(),
         name: request.name,
         kana_name: request.kana_name,
@@ -97,7 +98,7 @@ pub async fn handler(ctx: Login<Context>, request: Request) -> HandlerResult<Res
                 .collect()
         }),
     };
-    let project = update_any_project::run(&ctx, input).await?;
+    let project = update_project::run(&ctx, input).await?;
     let project = Project::from_use_case(project);
     Ok(Response { project })
 }
