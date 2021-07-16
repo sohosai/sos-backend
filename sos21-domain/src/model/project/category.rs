84327be5b05e947930bebfeb370d9e6b26d48916
coord_e <me@coord-e.com>
2021-07-16 06:32:11 +0000

    Add: Implement ProjectQuery::possible_categories

--- sos21-domain/src/model/project/category.rs
+++ sos21-domain/src/model/project/category.rs
@@ -1,9 +1,22 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum ProjectCategory {
     General,
     Stage,
     Cooking,
     Food,
+}
+
+impl ProjectCategory {
+    pub fn enumerate() -> impl Iterator<Item = ProjectCategory> {
+        [
+            ProjectCategory::General,
+            ProjectCategory::Stage,
+            ProjectCategory::Cooking,
+            ProjectCategory::Food,
+        ]
+        .iter()
+        .copied()
+    }
 }
