383a0f93a201413f4897da104077a055c28f68f9
coord_e <me@coord-e.com>
2021-08-28 06:10:13 +0000

    Add: Add test cases for FileFormItem

--- sos21-domain/src/model/form/item/file.rs
+++ sos21-domain/src/model/form/item/file.rs
@@ -62,3 +62,325 @@ impl FileFormItem {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{CheckAnswerErrorKind, FileFormItem, FileFormItemTypes};
+    use crate::test::model as test_model;
+
+    #[test]
+    fn test_answer_single_optional_pass() {
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: false,
+            is_required: false,
+        };
+
+        item.check_answer(&FormAnswerItemFileSharings::from_sharing_answers(vec![]).unwrap())
+            .unwrap();
+        item.check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![FileSharingAnswer {
+                sharing_id: test_model::new_file_sharing_id(),
+                type_: test_model::mock_file_type(),
+            }])
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_answer_single_optional_fail_multiple() {
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: false,
+            is_required: false,
+        };
+
+        assert_eq!(
+            item.check_answer(
+                &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                    FileSharingAnswer {
+                        sharing_id: test_model::new_file_sharing_id(),
+                        type_: test_model::mock_file_type(),
+                    },
+                    FileSharingAnswer {
+                        sharing_id: test_model::new_file_sharing_id(),
+                        type_: test_model::mock_file_type(),
+                    },
+                ])
+                .unwrap(),
+            )
+            .unwrap_err()
+            .kind(),
+            CheckAnswerErrorKind::NotAllowedMultipleFiles
+        );
+    }
+
+    #[test]
+    fn test_answer_multiple_optional_pass() {
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: true,
+            is_required: false,
+        };
+
+        item.check_answer(&FormAnswerItemFileSharings::from_sharing_answers(vec![]).unwrap())
+            .unwrap();
+        item.check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![FileSharingAnswer {
+                sharing_id: test_model::new_file_sharing_id(),
+                type_: test_model::mock_file_type(),
+            }])
+            .unwrap(),
+        )
+        .unwrap();
+        item.check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: test_model::mock_file_type(),
+                },
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: test_model::mock_file_type(),
+                },
+            ])
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_answer_single_required_pass() {
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: false,
+            is_required: true,
+        };
+
+        item.check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![FileSharingAnswer {
+                sharing_id: test_model::new_file_sharing_id(),
+                type_: test_model::mock_file_type(),
+            }])
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_answer_single_required_fail_empty() {
+        use crate::model::form_answer::item::FormAnswerItemFileSharings;
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: false,
+            is_required: true,
+        };
+
+        assert_eq!(
+            item.check_answer(&FormAnswerItemFileSharings::from_sharing_answers(vec![]).unwrap(),)
+                .unwrap_err()
+                .kind(),
+            CheckAnswerErrorKind::NotAnswered
+        );
+    }
+
+    #[test]
+    fn test_answer_single_required_fail_multiple() {
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: false,
+            is_required: true,
+        };
+
+        assert_eq!(
+            item.check_answer(
+                &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                    FileSharingAnswer {
+                        sharing_id: test_model::new_file_sharing_id(),
+                        type_: test_model::mock_file_type(),
+                    },
+                    FileSharingAnswer {
+                        sharing_id: test_model::new_file_sharing_id(),
+                        type_: test_model::mock_file_type(),
+                    },
+                ])
+                .unwrap(),
+            )
+            .unwrap_err()
+            .kind(),
+            CheckAnswerErrorKind::NotAllowedMultipleFiles
+        );
+    }
+
+    #[test]
+    fn test_answer_multiple_required_pass() {
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: true,
+            is_required: true,
+        };
+
+        item.check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![FileSharingAnswer {
+                sharing_id: test_model::new_file_sharing_id(),
+                type_: test_model::mock_file_type(),
+            }])
+            .unwrap(),
+        )
+        .unwrap();
+        item.check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: test_model::mock_file_type(),
+                },
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: test_model::mock_file_type(),
+                },
+            ])
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_answer_multiple_required_fail_empty() {
+        use crate::model::form_answer::item::FormAnswerItemFileSharings;
+
+        let item = FileFormItem {
+            types: None,
+            accept_multiple_files: true,
+            is_required: true,
+        };
+
+        assert_eq!(
+            item.check_answer(&FormAnswerItemFileSharings::from_sharing_answers(vec![]).unwrap(),)
+                .unwrap_err()
+                .kind(),
+            CheckAnswerErrorKind::NotAnswered
+        );
+    }
+
+    #[test]
+    fn test_answer_types_pass() {
+        use crate::model::file::FileType;
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let type1 = FileType::from_mime(mime::IMAGE_PNG);
+        let type2 = FileType::from_mime(mime::IMAGE_JPEG);
+
+        FileFormItem {
+            types: Some(FileFormItemTypes::from_types(vec![type1.clone()]).unwrap()),
+            accept_multiple_files: true,
+            is_required: false,
+        }
+        .check_answer(&FormAnswerItemFileSharings::from_sharing_answers(vec![]).unwrap())
+        .unwrap();
+
+        FileFormItem {
+            types: Some(FileFormItemTypes::from_types(vec![type1.clone()]).unwrap()),
+            accept_multiple_files: true,
+            is_required: false,
+        }
+        .check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: type1.clone(),
+                },
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: type1.clone(),
+                },
+            ])
+            .unwrap(),
+        )
+        .unwrap();
+
+        FileFormItem {
+            types: Some(FileFormItemTypes::from_types(vec![type1.clone(), type2.clone()]).unwrap()),
+            accept_multiple_files: true,
+            is_required: false,
+        }
+        .check_answer(
+            &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: type1,
+                },
+                FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: type2,
+                },
+            ])
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_answer_types_fail() {
+        use crate::model::file::FileType;
+        use crate::model::form_answer::item::{FileSharingAnswer, FormAnswerItemFileSharings};
+
+        let type1 = FileType::from_mime(mime::IMAGE_PNG);
+        let type2 = FileType::from_mime(mime::IMAGE_JPEG);
+
+        assert_eq!(
+            FileFormItem {
+                types: Some(FileFormItemTypes::from_types(vec![type2.clone()]).unwrap()),
+                accept_multiple_files: true,
+                is_required: false,
+            }
+            .check_answer(
+                &FormAnswerItemFileSharings::from_sharing_answers(vec![FileSharingAnswer {
+                    sharing_id: test_model::new_file_sharing_id(),
+                    type_: type1.clone(),
+                },])
+                .unwrap(),
+            )
+            .unwrap_err()
+            .kind(),
+            CheckAnswerErrorKind::NotAllowedFileType
+        );
+
+        assert_eq!(
+            FileFormItem {
+                types: Some(FileFormItemTypes::from_types(vec![type2.clone()]).unwrap()),
+                accept_multiple_files: true,
+                is_required: false,
+            }
+            .check_answer(
+                &FormAnswerItemFileSharings::from_sharing_answers(vec![
+                    FileSharingAnswer {
+                        sharing_id: test_model::new_file_sharing_id(),
+                        type_: type1,
+                    },
+                    FileSharingAnswer {
+                        sharing_id: test_model::new_file_sharing_id(),
+                        type_: type2,
+                    },
+                ])
+                .unwrap(),
+            )
+            .unwrap_err()
+            .kind(),
+            CheckAnswerErrorKind::NotAllowedFileType
+        );
+    }
+}
--- sos21-domain/src/test/model/file_sharing.rs
+++ sos21-domain/src/test/model/file_sharing.rs
@@ -6,12 +6,16 @@ use crate::model::file_sharing::{
 
 use uuid::Uuid;
 
+pub fn new_file_sharing_id() -> FileSharingId {
+    FileSharingId::from_uuid(Uuid::new_v4())
+}
+
 pub fn new_expired_file_sharing(file_id: FileId, scope: FileSharingScope) -> FileSharing {
     let expires_at = DateTime::now();
     let created_at = DateTime::from_utc(expires_at.utc() - chrono::Duration::seconds(1));
 
     let sharing = FileSharingContent {
-        id: FileSharingId::from_uuid(Uuid::new_v4()),
+        id: new_file_sharing_id(),
         created_at,
         file_id,
         is_revoked: false,
