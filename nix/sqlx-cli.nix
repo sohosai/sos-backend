68392283909048dae75182a2cfb22081ea87d03c
momeemt <momiimt@gmail.com>
2023-04-22 14:12:02 +0900

    rust、sqlx、sqlx-cliのアップデート

--- nix/sqlx-cli.nix
+++ nix/sqlx-cli.nix
@@ -1,15 +1,15 @@
 { rustPlatform, fetchFromGitHub, openssl, pkg-config, lib }:
 rustPlatform.buildRustPackage rec {
   pname = "sqlx-cli";
-  version = "0.5.1";
+  version = "0.6.3";
   src = builtins.fetchTarball {
-    url = "https://github.com/launchbadge/sqlx/archive/v0.5.1.tar.gz";
-    sha256 = "02phkrcjszs6gdq0yva9fv9f8c4bda0vp9alml7kr5fj65gns8mh";
+    url = "https://github.com/launchbadge/sqlx/archive/v0.6.3.tar.gz";
+    sha256 = "11j8vjb9dz551894379gikw6blsaqdchkx19gl62rzbkfcfrpcmc";
   };
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
   cargoBuildFlags = [ "--manifest-path=sqlx-cli/Cargo.toml" ];
-  cargoSha256 = "1899jwqvdrsdhncg107k0i3w8l496gz0d73zdj2mxnj2lfmpfq0s";
+  cargoSha256 = "WRVrxBQytxogdaiG6KTmSTaSzN1gh9cltYkVEf4wUUI=";
   doCheck = false;
   meta = with lib; {
     description = "Command-line utility for SQLx, the Rust SQL toolkit.";
